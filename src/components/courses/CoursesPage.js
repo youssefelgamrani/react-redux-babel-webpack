4cc0581644c2bc3718265707708d7afed2e94e37
gamrani <gamraniyoussef@gmail.com>
2020-04-05 14:08:35 +0100

    add create course form

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
 
 export default class CoursesPage extends Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    console.log(course);
+    this.setState({ course: course });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.course.title);
+  };
+
   render() {
-    return <h2>Devteam school</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="save" />
+      </form>
+    );
   }
 }
