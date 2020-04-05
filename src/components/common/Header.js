aa00adb431db13aa2302a9add80c967df1faecd3
gamrani <gamraniyoussef@gmail.com>
2020-04-05 01:03:55 +0100

    add common folder and header component

---
+++ src/components/common/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+  const activeStyle = { color: "#F15B2A" };
+
+  return (
+    <nav>
+      <NavLink exact to="/" activeStyle={activeStyle}>
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink to="/about" activeStyle={activeStyle}>
+        About
+      </NavLink>
+    </nav>
+  );
+}
