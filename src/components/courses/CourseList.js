24696ec303806d540def76b2b03efa01992f8bf6
gamrani <gamraniyoussef@gmail.com>
2020-04-05 22:35:34 +0100

    add course list and midlleware

---
+++ src/components/courses/CourseList.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+
+function CourseList({ courses }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th />
+          <th>Title</th>
+          <th>Author</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {courses.map((course) => {
+          return (
+            <tr key={course.id}>
+              <td>
+                <a className="btn btn-light" href="www.google.com">
+                  Watch
+                </a>
+              </td>
+              <td>
+                <Link to={"/course/" + course.slug}>{course.title}</Link>
+              </td>
+              <td>{course.authorId}</td>
+              <td>{course.category}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+CourseList.propTypes = {
+  courses: PropTypes.isrequired,
+};
+
+export default CourseList;
--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -3,40 +3,21 @@ import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
 
 class CoursesPage extends Component {
-  state = {
-    course: {
-      title: "",
-    },
-  };
-
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course: course });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    debugger;
-    this.props.courseActions.createCourse(this.state.course);
-  };
+  componentDidMount() {
+    this.props.courseActions.loadCourses().catch((error) => {
+      alert("Loading courses failed " + error);
+    });
+  }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="save" />
-        {this.props.courses.map((course) => (
-          <div key={course.title}>{course.title}</div>
-        ))}
-      </form>
+        <CourseList courses={this.props.courses} />
+      </>
     );
   }
 }
